dd9b9ce20b18fd975a966e4a7a5cca4657b430f2
Vipin Pathak <vipinpathak018@gmail.com>
2025-08-12 13:17:48 +0530

    ball boucne animation

--- src/main.js
+++ src/main.js
@@ -1,11 +1,26 @@
-// import * as animeModule from 'animejs/lib/anime.esm.js';
-// const anime = animeModule.default || animeModule;
 
-anime({
-  targets: '.box',
+/**
+ * BALL BOUNCE ANIMATION
+  
+ anime({
+targets: '.box',
   translateY: [
-    { value: 300, duration: 500, easing: 'easeInQuad' },  // fall
-    { value: 0, duration: 500, easing: 'easeOutQuad' }    // rise
+    { value: 350, duration: 500, easing: 'easeInQuad' },  // fall
+    { value: 0, duration: 800, easing: 'easeOutElastic(1, 0.5)' } // spring up
+  ],
+  scaleX: [
+    { value: 1.15, duration: 100, delay: 450, easing: 'easeOutQuad' }, // squash
+    { value: 1, duration: 800, easing: 'easeOutElastic(1, 0.5)' }
+  ],
+  scaleY: [
+    { value: 0.8, duration: 100, delay: 450, easing: 'easeOutQuad' }, // squash
+    { value: 1, duration: 800, easing: 'easeOutElastic(1, 0.5)' }
+  ],
+  rotate: [
+    { value: 5, duration: 500, easing: 'easeInQuad' },   // tilt down
+    { value: -5, duration: 800, easing: 'easeOutElastic(1, 0.5)' } // tilt up
   ],
   loop: true
 });
+
+ */
